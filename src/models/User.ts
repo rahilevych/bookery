7a4fe33342bf5d23d688b686e321a4552f3c5c4c
AlonaRahilevych <ragilevic@gmail.com>
2024-07-30 13:35:03 +0200

    User schema was created

---
+++ src/models/User.ts
@@ -0,0 +1,106 @@
+import mongoose, { Schema, model } from 'mongoose';
+export interface UserDocument {
+  _id: string;
+  email: string;
+  password: string;
+  username: string;
+  name: string;
+  surname: string;
+  dateOfBirth: Date;
+  country: string;
+  city: string;
+  postcode: number;
+  avatar: string;
+  createdAt: Date;
+  savedBooks: [string];
+  likedBooks: [string];
+  boughtBooks: [string];
+  orders: [string];
+}
+
+const UserSchema = new Schema<UserDocument>({
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Email is required'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Email is invalid',
+    ],
+  },
+  password: {
+    type: String,
+    unique: false,
+    required: [true, 'Password is required'],
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: [true, 'Username is required'],
+  },
+  name: {
+    type: String,
+    unique: false,
+    required: [true, 'Name is required'],
+  },
+  surname: {
+    type: String,
+    unique: false,
+    required: [true, 'Surname is required'],
+  },
+
+  dateOfBirth: {
+    type: Date,
+    unique: false,
+    required: false,
+  },
+  country: {
+    type: String,
+    unique: false,
+    required: false,
+  },
+  city: {
+    type: String,
+    unique: false,
+    required: false,
+  },
+  postcode: {
+    type: Number,
+    unique: false,
+    required: false,
+  },
+
+  avatar: {
+    type: String,
+    unique: false,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    unique: false,
+    required: true,
+    default: Date.now,
+  },
+  savedBooks: {
+    type: [String],
+    unique: false,
+    required: false,
+  },
+  likedBooks: {
+    type: [String],
+    unique: false,
+    required: false,
+  },
+  boughtBooks: {
+    type: [String],
+    unique: false,
+    required: false,
+  },
+  orders: {
+    type: [String],
+    unique: false,
+    required: false,
+  },
+});
+const User = mongoose.models?.User || model<UserDocument>('User', UserSchema);
+export default User;
