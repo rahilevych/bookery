dc6d5bf893d8c4fe60c28a4a6ad98baf2a78ef3b
AlonaRahilevych <ragilevic@gmail.com>
2024-08-01 14:56:17 +0200

    Navbar component was created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React from 'react';
+import Search from './Search';
+import logo from '../img/logo.png';
+import { Heart, ShoppingCart, User } from '@phosphor-icons/react';
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <div className='  bg-white w-full h-28'>
+      <div className=' container mx-auto flex flex-row items-center justify-between  h-full '>
+        {' '}
+        <div className='w-16 h-auto'>
+          <img src={logo.src} alt='' />
+        </div>
+        <Search />
+        <div className='flex flex-row gap-4 '>
+          <div className='bg-[#F4F4FF] h-10 w-10 flex items-center justify-center rounded'>
+            <User size={32} />
+          </div>
+          <div className='bg-[#F4F4FF] h-10 w-10 flex items-center justify-center rounded'>
+            <Heart size={32} />
+          </div>
+          <div className='bg-[#F4F4FF] h-10 w-10 flex items-center justify-center rounded'>
+            <ShoppingCart size={32} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
