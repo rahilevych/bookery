d25df36815b2d8783fb90722a09c6dd03422b26c
AlonaRahilevych <ragilevic@gmail.com>
2024-08-07 11:32:01 +0200

    BookPreview component was changed

--- src/components/BookPreview.tsx
+++ src/components/BookPreview.tsx
@@ -1,18 +1,22 @@
 'use client';
 import React from 'react';
 import bookImg from '../img/book.png';
-type Props = {};
+type Props = {
+  img: string;
+  author: string;
+  title: string;
+};
 
 const BookPreview = (props: Props) => {
   return (
     <div className='flex flex-row items-center justify-between w-80 h-52 p-2 bg-[#F4F4FF]'>
       <div className='flex items-center justify-center '>
-        <img src={bookImg.src} alt='' className='h-full w-auto' />
+        <img src={props.img} alt='' className='h-full w-auto' />
       </div>
       <div className='flex flex-col items-start justify-between h-full'>
         <div>
-          <p className='text-[#090937] font-semibold'>Dune</p>
-          <p className='text-[#09093799] font-semibold'>Frank Herbert</p>
+          <p className='text-[#090937] font-semibold'>{props.title}</p>
+          <p className='text-[#09093799] font-semibold'>{props.author}</p>
         </div>
         <p className='text-[#6251DD] font-bold'>87,75$</p>
       </div>
