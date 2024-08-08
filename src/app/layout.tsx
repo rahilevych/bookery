82a3bb998eee095bf435f9ccad50b80dbc00d1af
AlonaRahilevych <ragilevic@gmail.com>
2024-08-08 10:38:52 +0200

    book wrapper was added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Provider } from './provider';
+import { BooksWrapper } from '@/context/BookContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <Provider>
-        <body className={inter.className}>{children}</body>
+        <BooksWrapper>
+          {' '}
+          <body className={inter.className}>{children}</body>
+        </BooksWrapper>
       </Provider>
     </html>
   );
