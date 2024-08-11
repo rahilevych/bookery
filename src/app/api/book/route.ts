4eeef1dce86d33805dbea706ebc4488f2f2f5419
AlonaRahilevych <ragilevic@gmail.com>
2024-08-11 13:46:58 +0200

    route for getting book by Id was created

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,28 +1,36 @@
 import { connectDB } from '@/lib/mongodb';
 import Book from '@/models/Book';
+import mongoose from 'mongoose';
+import { NextApiRequest } from 'next';
+import { SearchParamsContext } from 'next/dist/shared/lib/hooks-client-context.shared-runtime';
+
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest, res: NextResponse) {
-  const searchParams = req.nextUrl.searchParams;
-  const query = searchParams.get('query');
+type Params = {
+  bookId: string;
+};
 
-  console.log('searchParams>>>>', searchParams);
+export async function GET(req: Request, res: NextResponse) {
+  //const searchParams = req.nextUrl.searchParams;
+  //const bookId = searchParams.get('id');
+  //console.log(bookId);
+  //console.log(req);
+  //const bookId = context.params.bookId;
+
+  //console.log('searchParams>>>>', bookId);
+  // console.log('req>>>>', req);
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get('id');
+  // const { id } = req.query;
+  console.log('searchParams>>>>', id);
   try {
     await connectDB();
-    // const input = parseInt(query as string, 10) || '';
-    const input = {
-      $or: [
-        { authors: query },
-        { title: query },
-        { categories: query },
-        { subtitle: query },
-      ],
-    };
-    const books = await Book.find(input);
 
-    return Response.json({ books: books });
+    const book = await Book.findById(id);
+    console.log(book);
+    return Response.json({ book: book });
   } catch (error) {
-    console.error('Error by getting list of books', error);
+    console.error('Error by getting book', error);
     return Response.json({ error: 'Server error' });
   }
 }
