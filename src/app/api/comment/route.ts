58c5db418faeff3451f8009b69999cf86b464250
AlonaRahilevych <ragilevic@gmail.com>
2024-08-11 13:45:31 +0200

    route for deleting comment was created

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -29,3 +29,30 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: 'Server error' }, { status: 500 });
   }
 }
+export async function DELETE(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(req.url);
+    const commentId = searchParams.get('commentId');
+    const bookId = searchParams.get('bookId');
+    const userId = searchParams.get('userId');
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      return NextResponse.json({ error: 'Comment not found' }, { status: 404 });
+    }
+    if (comment.user_id.toString() !== userId) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 403 });
+    }
+    await comment.deleteOne();
+
+    await Book.findByIdAndUpdate(bookId, {
+      $pull: { comments: comment._id },
+    });
+
+    return NextResponse.json({ comment });
+  } catch (error) {
+    console.error('Error by deleting comment:', error);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
