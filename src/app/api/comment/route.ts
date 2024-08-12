123c416c11c04cdf87526bd83c28ab8437bcc1ea
AlonaRahilevych <ragilevic@gmail.com>
2024-08-12 22:19:19 +0200

    comment routes for adding deleting and fetching all comments were created

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -1,8 +1,28 @@
 import { connectDB } from '@/lib/mongodb';
 import Book from '@/models/Book';
 import Comment from '@/models/Comment';
+import { User } from '@phosphor-icons/react';
+import { getToken } from 'next-auth/jwt';
 
 import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(req.url);
+    const bookId = searchParams.get('bookId');
+
+    if (!bookId) {
+      return NextResponse.json({ error: 'Missing bookId' }, { status: 400 });
+    }
+    const comments = await Comment.find({ book_id: bookId }).exec();
+    return NextResponse.json({ comments: comments });
+  } catch (error) {
+    console.error('Error by getting comments:', error);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
 
 export async function POST(req: NextRequest) {
   try {
@@ -10,19 +30,21 @@ export async function POST(req: NextRequest) {
 
     const { searchParams } = new URL(req.url);
     const bookId = searchParams.get('bookId');
-
+    const userId = searchParams.get('userId');
+    console.log('user id>>>>' + userId);
     const body = await req.json();
-
+    console.log('user id>>>>' + userId);
+    if (!userId) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
     const newComment = {
-      user_id: 'userId',
+      user_id: userId,
       book_id: bookId,
       text: body.text,
     };
 
     const comment = await Comment.create(newComment);
-
     await Book.findByIdAndUpdate(bookId, { $push: { comments: comment._id } });
-
     return NextResponse.json({ comment });
   } catch (error) {
     console.error('Error by adding comment:', error);
@@ -32,11 +54,15 @@ export async function POST(req: NextRequest) {
 export async function DELETE(req: NextRequest) {
   try {
     await connectDB();
-
     const { searchParams } = new URL(req.url);
     const commentId = searchParams.get('commentId');
     const bookId = searchParams.get('bookId');
     const userId = searchParams.get('userId');
+
+    if (!userId) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
     const comment = await Comment.findById(commentId);
     if (!comment) {
       return NextResponse.json({ error: 'Comment not found' }, { status: 404 });
