28b6ddd35d7499df43d1037871a3d3c92a7820d0
AlonaRahilevych <ragilevic@gmail.com>
2024-08-12 22:23:56 +0200

    wishlist routes were created

---
+++ src/app/api/wishlist/route.ts
@@ -0,0 +1,82 @@
+import { connectDB } from '@/lib/mongodb';
+import Book from '@/models/Book';
+import User from '@/models/User';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+
+    if (!userId) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await User.findById(userId).select('likedBooks');
+    console.log('user>>>>>>>>>>>>>>>>' + user);
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    const likedBooks = user.likedBooks;
+    console.log('likedBooks>>>>' + likedBooks);
+
+    const books = await Book.find({ _id: { $in: likedBooks } });
+    console.log('likedBooks for wishlist' + books);
+    return NextResponse.json({ books: books });
+  } catch (error) {
+    console.error('Error by getting books:', error);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+    const bookId = searchParams.get('bookId');
+
+    if (!userId || !bookId) {
+      return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+    const book = await Book.findById(bookId);
+    if (!book) {
+      return NextResponse.json({ error: 'Book not found' }, { status: 404 });
+    }
+
+    if (!Array.isArray(user.likedBooks) && !Array.isArray(book.likes)) {
+      user.likedBooks = [];
+      book.likes = [];
+    }
+
+    let isLiked =
+      user.likedBooks.includes(bookId) && book.likes.includes(userId);
+    console.log('isLiked>>>>' + isLiked);
+
+    if (isLiked) {
+      user.likedBooks = user.likedBooks.filter(
+        (id: any) => id.toString() !== bookId
+      );
+      book.likes = book.likes.filter((id: any) => id.toString() !== userId);
+    } else {
+      user.likedBooks.push(bookId);
+      book.likes.push(userId);
+    }
+    await user.save();
+    await book.save();
+    return NextResponse.json({ isLiked: !isLiked }, { status: 200 });
+  } catch (error) {
+    console.error('Error in POST handler:', error);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
