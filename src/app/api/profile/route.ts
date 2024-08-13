2a4b816f1aee6facafde076053073c1f04a06c82
AlonaRahilevych <ragilevic@gmail.com>
2024-08-13 23:18:47 +0200

    profile routes were created

---
+++ src/app/api/profile/route.ts
@@ -0,0 +1,61 @@
+import { cloudinary } from '@/config/cloudinary';
+import { connectDB } from '@/lib/mongodb';
+import User from '@/models/User';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+
+    if (!userId) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.error('Error while getting user:', error);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+
+    if (!userId) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const body = await req.json();
+
+    console.log('body avatar>>>>>>' + body.avatar);
+    if (body.avatar) {
+      const img = await cloudinary.uploader.upload(body.avatar, {
+        upload_preset: 'user_preset',
+      });
+      body.avatar = img.secure_url;
+    }
+
+    const user = await User.findByIdAndUpdate(userId, body);
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({ user: user });
+  } catch (error) {
+    console.error('Error while updating user:', error);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
