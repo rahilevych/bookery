50ac1434730937b0e59e4a4ffcd2832b15a7a450
AlonaRahilevych <ragilevic@gmail.com>
2024-08-16 10:27:10 +0200

    registe function was updated

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,20 +1,28 @@
 'use server';
+
 import { connectDB } from '@/lib/mongodb';
 import User from '@/models/User';
 import bcrypt from 'bcryptjs';
 
-export const register = async (values: any) => {
+export const register = async (values: {
+  email: string;
+  password: string;
+  name: string;
+  surname: string;
+  username: string;
+}) => {
   const { email, password, name, surname, username } = values;
 
   try {
     await connectDB();
+
     const userFound = await User.findOne({ email });
     if (userFound) {
-      return {
-        error: 'Email already exists!',
-      };
+      return { error: 'Email already exists!' };
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = new User({
       email,
       username,
@@ -22,8 +30,12 @@ export const register = async (values: any) => {
       surname,
       password: hashedPassword,
     });
-    const savedUser = await user.save();
+
+    await user.save();
+
+    return { success: 'User registered successfully!' };
   } catch (e) {
-    console.log(e);
+    console.error('Error during registration:', e);
+    return { error: 'An unexpected error occurred during registration' };
   }
 };
