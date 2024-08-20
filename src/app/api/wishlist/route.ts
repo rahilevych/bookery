ff4f6c408de1b310e56aea4e1cd96a7f7def90f7
AlonaRahilevych <ragilevic@gmail.com>
2024-08-20 08:36:37 +0200

    logs in wishlist were deleted

--- src/app/api/wishlist/route.ts
+++ src/app/api/wishlist/route.ts
@@ -15,17 +15,15 @@ export async function GET(req: NextRequest) {
     }
 
     const user = await User.findById(userId).select('likedBooks');
-    console.log('user>>>>>>>>>>>>>>>>' + user);
 
     if (!user) {
       return NextResponse.json({ error: 'User not found' }, { status: 404 });
     }
 
     const likedBooks = user.likedBooks;
-    console.log('likedBooks>>>>' + likedBooks);
 
     const books = await Book.find({ _id: { $in: likedBooks } });
-    console.log('likedBooks for wishlist' + books);
+
     return NextResponse.json({ books: books });
   } catch (error) {
     console.error('Error by getting books:', error);
@@ -61,7 +59,6 @@ export async function POST(req: NextRequest) {
 
     let isLiked =
       user.likedBooks.includes(bookId) && book.likes.includes(userId);
-    console.log('isLiked>>>>' + isLiked);
 
     if (isLiked) {
       user.likedBooks = user.likedBooks.filter(
@@ -77,6 +74,45 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ isLiked: !isLiked }, { status: 200 });
   } catch (error) {
     console.error('Error in POST handler:', error);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
+export async function DELETE(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(req.url);
+    const userId = searchParams.get('userId');
+    const bookId = searchParams.get('bookId');
+
+    if (!userId || !bookId) {
+      return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+    const book = await Book.findById(bookId);
+    if (!book) {
+      return NextResponse.json({ error: 'Book not found' }, { status: 404 });
+    }
+
+    user.likedBooks = user.likedBooks.filter(
+      (id: any) => id.toString() !== bookId
+    );
+
+    book.likes = book.likes.filter((id: any) => id.toString() !== userId);
+
+    await user.save();
+    await book.save();
+
+    return NextResponse.json(
+      { message: 'Book removed from wishlist' },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error in DELETE handler:', error);
     return NextResponse.json({ error: 'Server error' }, { status: 500 });
   }
 }
