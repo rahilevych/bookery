a5e3741902442e6a74cb93c2a8fb8a237c526fba
AlonaRahilevych <ragilevic@gmail.com>
2024-08-20 08:37:16 +0200

    book context was updated

--- src/context/BookContext.tsx
+++ src/context/BookContext.tsx
@@ -2,13 +2,8 @@
 import { BookDocument } from '@/models/Book';
 import { Book } from '@/types/types';
 import { useRouter, useSearchParams } from 'next/navigation';
-import {
-  ReactNode,
-  createContext,
-  useContext,
-  useReducer,
-  useState,
-} from 'react';
+import { ReactNode, createContext, useContext, useState } from 'react';
+
 type BookTypeContext = {
   input: string;
   setInput: (input: string) => void;
@@ -18,15 +13,27 @@ type BookTypeContext = {
   setPageNumber: (number: number) => void;
   fetchBooks: () => Promise<void>;
   searchBooks: () => Promise<void>;
+  checkIfLiked: (userId: string, bookId: string) => Promise<void>;
+  checkIfInCart: (userId: string, bookId: string) => Promise<void>;
   books: Book[];
   book: Book | null;
-
   setBook: (book: Book) => void;
+  setIsLiked: (isLiked: boolean) => void;
+  setIsInCart: (isInCart: boolean) => void;
   fetchOneBook: (bookId: string) => Promise<void>;
+  loading: boolean;
+  isInCart: boolean;
+  isLiked: boolean;
 };
 const BooksContext = createContext<BookTypeContext>({
   input: '',
   setInput: () => {
+    throw new Error('context not initialised');
+  },
+  setIsInCart: () => {
+    throw new Error('context not initialised');
+  },
+  setIsLiked: () => {
     throw new Error('context not initialised');
   },
   bookId: '',
@@ -41,10 +48,15 @@ const BooksContext = createContext<BookTypeContext>({
   fetchBooks: () => Promise.resolve(),
   books: [],
   fetchOneBook: () => Promise.resolve(),
+  checkIfInCart: () => Promise.resolve(),
+  checkIfLiked: () => Promise.resolve(),
   setBook: () => {
     throw new Error('context not initialised');
   },
   book: null,
+  loading: false,
+  isLiked: false,
+  isInCart: false,
 });
 
 export function BooksWrapper({ children }: { children: React.ReactNode }) {
@@ -54,9 +66,12 @@ export function BooksWrapper({ children }: { children: React.ReactNode }) {
   const [amountOfItems, setAmountOfItems] = useState(20);
   const [bookId, setBookId] = useState('');
   const [book, setBook] = useState<Book | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isLiked, setIsLiked] = useState(false);
+  const [isInCart, setIsInCart] = useState(false);
 
   async function fetchBooks() {
-    console.log('pageB sizeB', pageNumber, amountOfItems);
+    setLoading(true);
     try {
       const res = await fetch(
         `/api/books?page=${pageNumber}&size=${amountOfItems}&query=${input}`
@@ -68,6 +83,8 @@ export function BooksWrapper({ children }: { children: React.ReactNode }) {
       const data = await res.json();
 
       setBooks(data.books);
+
+      setLoading(false);
     } catch (error) {
       console.error('Error by fetching books', error);
     }
@@ -75,14 +92,9 @@ export function BooksWrapper({ children }: { children: React.ReactNode }) {
 
   async function fetchOneBook(id: string) {
     try {
-      //const searchParams = useSearchParams();
-
-      //const search = searchParams.get('bookId');
-
-      //const { bookId } = search;
       console.log('id>>>>' + id);
       setBookId(id.toString());
-      const res = await fetch(`/api/book?id=${id}`);
+      const res = await fetch(`/api/books/book?id=${id}`);
 
       if (!res.ok) {
         throw new Error('Erorr by getting book');
@@ -94,10 +106,9 @@ export function BooksWrapper({ children }: { children: React.ReactNode }) {
       console.error('Error by getting book', error);
     }
   }
-  console.log('book>>>>>' + book);
   async function searchBooks() {
     try {
-      const res = await fetch(`/api/book?query=${input}`);
+      const res = await fetch(`/api/books/book?query=${input}`);
 
       if (!res.ok) {
         throw new Error('Erorr by fetching books');
@@ -110,6 +121,51 @@ export function BooksWrapper({ children }: { children: React.ReactNode }) {
       console.error('Error by fetching books', error);
     }
   }
+
+  const checkIfLiked = async (userId: string, bookId: string) => {
+    try {
+      if (!userId || !bookId) return;
+
+      const response = await fetch(
+        `/api/books/book/isLiked?bookId=${bookId}&userId=${userId}`,
+        {
+          method: 'GET',
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to check if book is liked');
+      }
+
+      const data = await response.json();
+      setIsLiked(data.isLiked);
+    } catch (error) {
+      console.error('Failed to check if book is liked:', error);
+    }
+  };
+
+  const checkIfInCart = async (userId: string, bookId: string) => {
+    try {
+      if (!userId || !bookId) return;
+
+      const response = await fetch(
+        `/api/cart/isInCart?bookId=${bookId}&userId=${userId}`,
+        {
+          method: 'GET',
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Failed to check if book is in cart');
+      }
+
+      const data = await response.json();
+      setIsInCart(data.isInCart);
+    } catch (error) {
+      console.error('Failed to check if book is in cart:', error);
+    }
+  };
+
   return (
     <BooksContext.Provider
       value={{
@@ -125,6 +181,13 @@ export function BooksWrapper({ children }: { children: React.ReactNode }) {
         setBook,
         fetchOneBook,
         book,
+        loading,
+        checkIfInCart,
+        checkIfLiked,
+        setIsInCart,
+        setIsLiked,
+        isLiked,
+        isInCart,
       }}>
       {children}
     </BooksContext.Provider>
