19cd55f782abb98cb237848bc7d2ddf8371dc547
AlonaRahilevych <ragilevic@gmail.com>
2025-03-12 13:23:33 +0100

    refactor: update Search component to make it more cleare and to avoid dublшсфеу дщпшс

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,44 +1,34 @@
-import React, { useState, useCallback, useRef } from 'react';
-import { useApp } from '@/context/AppContext';
-import { fetchBooks } from '@/services/book.Service';
+import React, { useCallback, useRef } from 'react';
 
-const Search = () => {
-  const { setBooks } = useApp();
-  const [pageNumber, setPageNumber] = useState<number>(1);
-  const [searchTerm, setSearchTerm] = useState('');
-
+interface Props {
+  onInputChange: (text: string) => void;
+  text: string;
+}
+const Search: React.FC<Props> = ({ onInputChange, text }) => {
   const debounceTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  const loadSearchedBooks = async () => {
-    const books = await fetchBooks(pageNumber, 20, searchTerm);
-    setBooks(books);
-  };
   const debounceFetchBooks = useCallback(
     (value: string) => {
       if (debounceTimeoutRef.current) {
         clearTimeout(debounceTimeoutRef.current);
       }
       debounceTimeoutRef.current = setTimeout(() => {
-        setSearchTerm(value);
-        setPageNumber((prev) => prev);
-        loadSearchedBooks();
+        onInputChange(value);
       }, 300);
     },
-    [setSearchTerm, setPageNumber, loadSearchedBooks]
+    [onInputChange]
   );
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setSearchTerm(value);
     debounceFetchBooks(value);
-    setPageNumber(1);
+    onInputChange(value);
   };
 
   return (
     <div>
       <input
         type='text'
-        value={searchTerm}
+        value={text}
         onChange={handleInputChange}
         placeholder='Search for books...'
         className='w-full p-3 pl-12 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500'
